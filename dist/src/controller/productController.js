f427c784a3356e512ad5bb08a296c8f0508076c6
Jesus Corona <jesuscorona@MacBook-Air-34.local>
2026-03-11 13:08:59 -0700

    feat: implement product controller and CRUD endpoints

---
+++ dist/src/controller/productController.js
@@ -0,0 +1,124 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteProduct = exports.modifyProduct = exports.getProductById = exports.getAllProducts = exports.createProduct = void 0;
+const product_1 = require("../models/product");
+// Create new product
+const createProduct = (req, res) => {
+    // Validate request
+    if (!req.body) {
+        return res.status(400).json({
+            status: "error",
+            message: "Content can not be empty",
+            payload: null,
+        });
+    }
+    // Save Product in the database
+    const product = { ...req.body };
+    product_1.Product.create(product)
+        .then((data) => {
+        res.status(200).json({
+            status: "success",
+            message: "Product successfully created",
+            payload: data,
+        });
+    })
+        .catch((err) => {
+        res.status(500).json({
+            status: "error",
+            message: "Something happened creating a product. " + err.message,
+            payload: null,
+        });
+    });
+};
+exports.createProduct = createProduct;
+// Get all products using Promises
+const getAllProducts = (req, res) => {
+    //Calling the Sequelize findAll method. This is the same that a SELECT * FROM PRODUCT in a SQL query. 
+    product_1.Product.findAll()
+        .then((data) => {
+        return res.status(200).json({
+            status: "success",
+            message: "Products successfully retrieved",
+            payload: data,
+        });
+    })
+        .catch((err) => {
+        return res.status(500).json({
+            status: "error",
+            message: "Something happened retrieving all products. " + err.message,
+            payload: null,
+        });
+    });
+};
+exports.getAllProducts = getAllProducts;
+/// Get products by Id 
+const getProductById = (req, res) => {
+    product_1.Product.findByPk(req.params.id)
+        .then((data) => {
+        return res.status(200).json({
+            status: "success",
+            message: "Products successfully retrieved",
+            payload: data,
+        });
+    })
+        .catch((err) => {
+        return res.status(500).json({
+            status: "error",
+            message: "Something happened retrieving all products. " + err.message,
+            payload: null,
+        });
+    });
+};
+exports.getProductById = getProductById;
+///Modify product 
+const modifyProduct = (req, res) => {
+    // Validate request 
+    if (!req.body) {
+        return res.status(400).json({
+            status: "error",
+            message: "Content can not be empty.",
+            payload: null,
+        });
+    }
+    // Save Product in the database 
+    product_1.Product.update({ ...req.body }, { where: { id: req.params.id } })
+        .then((isUpdated) => {
+        if (isUpdated) {
+            return res.status(200).json({
+                status: "success",
+                message: "Product successfully updated",
+                payload: { ...req.body },
+            });
+        }
+        else {
+            return res.status(500).json({
+                status: "error",
+                message: "Something happened updating the product. ",
+                payload: null,
+            });
+        }
+    })
+        .catch((err) => {
+        res.status(500).json({
+            status: "error",
+            message: "Something happened updating a product. " + err.message,
+            payload: null,
+        });
+    });
+};
+exports.modifyProduct = modifyProduct;
+///Delete product
+const deleteProduct = async (req, res) => {
+    const { id } = req.body;
+    try {
+        await product_1.Product.destroy({ where: { id } });
+        res.status(200).json({ message: "Product deleted" });
+    }
+    catch (error) {
+        res.status(500).json({
+            message: "Error deleting products",
+            error,
+        });
+    }
+};
+exports.deleteProduct = deleteProduct;
